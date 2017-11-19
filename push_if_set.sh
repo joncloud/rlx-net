36055b2e0c977022ba670428e9ac97fe7fb1f828
joncloud <jdberube@gmail.com>
2017-11-19 09:17:44 -0800

    Adds bash script for uploading package

---
+++ push_if_set.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z $NUGET_API_KEY ]; then
+  echo "NuGet API Key not set"
+else
+  NUPKG_PATH=$(ls src/Rlx/bin/Release/*.nupkg)
+  if [ -z $NUPKG_PATH ]; then
+    echo "Missing NuPkg"
+    exit 1
+  elif [[ $NUPKG_PATH == *"local"* ]]; then
+    echo "Invalid Local NuPkg"
+    exit 2
+  else
+    echo "Uploading NuPkg"
+    dotnet nuget push $NUPKG_PATH -k $NUGET_API_KEY
+  fi
+fi
