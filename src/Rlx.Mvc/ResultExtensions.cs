80c1b613ed7c7a1df5e72d32cd0a628c1f0c62e2
joncloud <jdberube@gmail.com>
2017-11-21 19:39:41 -0800

    MVC extension ideas

---
+++ src/Rlx.Mvc/ResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Rlx.Mvc
+{
+    public static class ResultExtensions
+    {
+        public static IActionResult ToActionResult<TValue, TError>(this Result<TValue, TError> result)
+            => result.ToActionResult(_ => StatusCodes.Status200OK, _ => StatusCodes.Status400BadRequest);
+
+        public static IActionResult ToActionResult<TValue, TError>(this Result<TValue, TError> result, Func<TValue, int> ok, Func<TError, int> err)
+            => result.Map<IActionResult>(value => new ObjectResult(value) { StatusCode = ok(value) })
+                .UnwrapOrElse(error => new ObjectResult(error) { StatusCode = err(error) });
+
+        public static Task<IActionResult> ToActionResult<TValue, TError>(this ResultTask<TValue, TError> result)
+            => result.ToActionResult(_ => StatusCodes.Status200OK, _ => StatusCodes.Status400BadRequest);
+
+        public static Task<IActionResult> ToActionResult<TValue, TError>(this ResultTask<TValue, TError> result, Func<TValue, int> ok, Func<TError, int> err)
+            => result.Map<IActionResult>(value => new ObjectResult(value) { StatusCode = ok(value) })
+                .UnwrapOrElseAsync(error => new ObjectResult(error) { StatusCode = err(error) });
+    }
+}
