814fb429a5cdbe3032f5f393a65882f8b6f2fabe
joncloud <jdberube@gmail.com>
2017-11-22 11:18:43 -0800

    Adjusts extensions

--- src/Rlx.Mvc/OptionExtensions.cs
+++ src/Rlx.Mvc/OptionExtensions.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Rlx;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Rlx.Mvc
+namespace Microsoft.AspNetCore.Mvc
 {
     public static class OptionExtensions
     {
         public static IActionResult ToActionResult<T>(this Option<T> option)
             => option.MapOrElse<IActionResult>(() => new NotFoundResult(), value => new OkObjectResult(value));
-
+        
         public static Task<IActionResult> ToActionResult<T>(this OptionTask<T> option)
             => option.MapOrElse<IActionResult>(() => new NotFoundResult(), value => new OkObjectResult(value));
     }
