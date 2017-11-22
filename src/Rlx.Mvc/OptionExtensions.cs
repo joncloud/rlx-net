d4cae9568a49a32ace85369f40c467021bbdc580
joncloud <jdberube@gmail.com>
2017-11-21 19:39:41 -0800

    MVC extension ideas

---
+++ src/Rlx.Mvc/OptionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Rlx.Mvc
+{
+    public static class OptionExtensions
+    {
+        public static IActionResult ToActionResult<T>(this Option<T> option)
+            => option.MapOrElse<IActionResult>(() => new NotFoundResult(), value => new OkObjectResult(value));
+
+        public static Task<IActionResult> ToActionResult<T>(this OptionTask<T> option)
+            => option.MapOrElse<IActionResult>(() => new NotFoundResult(), value => new OkObjectResult(value));
+    }
+}
