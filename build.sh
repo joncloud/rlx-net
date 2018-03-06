fd42f4026567b25b08f25510ea660b0fb6d43fa1
joncloud <jdberube@gmail.com>
2018-03-05 20:25:02 -0800

    Adds beta flag

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 dotnet restore
 dotnet build --configuration=Release
 find ./tests -mindepth 1 -maxdepth 1 -type d -name '*.Tests' | xargs -L1 dotnet test --configuration=Release
-dotnet pack --configuration=Release --version-suffix="beta$TRAVIS_BUILD_ID"
+
+if [ -z $VERSION_SUFFIX ]; then
+  VERSION=$(grep -Po ./src/Rlx/Rlx.csproj -e '(?<=<VersionPrefix>)[^<]+')
+  dotnet pack --configuration=Release //p:Version=$VERSION
+else
+  dotnet pack --configuration=Release --version-suffix="$VERSION_SUFFIX$TRAVIS_BUILD_ID"
+fi
+
 ./push_if_set.sh
