b5be57fdc16683baec9fcd90e592fcadf88e932f
joncloud <jdberube@gmail.com>
2018-03-05 20:32:39 -0800

    Removes extra slash

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ find ./tests -mindepth 1 -maxdepth 1 -type d -name '*.Tests' | xargs -L1 dotnet 
 
 if [ -z $VERSION_SUFFIX ]; then
   VERSION=$(grep -Po ./src/Rlx/Rlx.csproj -e '(?<=<VersionPrefix>)[^<]+')
-  dotnet pack --configuration=Release //p:Version=$VERSION
+  dotnet pack --configuration=Release /p:Version=$VERSION
 else
   dotnet pack --configuration=Release --version-suffix="$VERSION_SUFFIX$TRAVIS_BUILD_ID"
 fi
