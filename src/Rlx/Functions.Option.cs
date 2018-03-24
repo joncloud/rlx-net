a9a0d22cc9f991b8765ddcc14b0e51635e9afb4b
joncloud <jdberube@gmail.com>
2018-03-24 11:10:58 -0700

    Moves option logic into its own file.

---
+++ src/Rlx/Functions.Option.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rlx
+{
+    public static partial class Functions
+    {
+        public static Option<T> None<T>()
+            => Option<T>.None;
+
+        public static OptionTask<T> NoneAsync<T>()
+            => OptionTask<T>.None;
+
+        public static Option<T> Some<T>(T value)
+            => new Option<T>(value);
+
+        public static Option<T> Some<T>(T value, IEqualityComparer<T> equalityComparer)
+            => new Option<T>(value, equalityComparer);
+
+        public static OptionTask<T> Some<T>(Task<T> task)
+            => new OptionTask<T>(task);
+
+        public static OptionTask<T> Some<T>(Func<Task<T>> fn)
+            => new OptionTask<T>(fn());
+    }
+}
--- src/Rlx/Functions.cs
+++ src/Rlx/Functions.cs
@@ -4,26 +4,8 @@ using System.Threading.Tasks;
 
 namespace Rlx
 {
-    public static class Functions
+    public static partial class Functions
     {
-        public static Option<T> None<T>()
-            => Option<T>.None;
-
-        public static OptionTask<T> NoneAsync<T>()
-            => OptionTask<T>.None;
-
-        public static Option<T> Some<T>(T value)
-            => new Option<T>(value);
-
-        public static Option<T> Some<T>(T value, IEqualityComparer<T> equalityComparer)
-            => new Option<T>(value, equalityComparer);
-
-        public static OptionTask<T> Some<T>(Task<T> task)
-            => new OptionTask<T>(task);
-
-        public static OptionTask<T> Some<T>(Func<Task<T>> fn)
-            => new OptionTask<T>(fn());
-
         public static Result<TValue, TError> Ok<TValue, TError>(TValue value)
             => new Result<TValue, TError>(value);
 
