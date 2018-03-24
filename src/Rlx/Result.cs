2d5aa9069788719885d15cf954b13be25ae46b66
joncloud <jdberube@gmail.com>
2018-03-24 11:44:56 -0700

    Moves to its own code file.

--- src/Rlx/Result.cs
+++ src/Rlx/Result.cs
@@ -196,13 +196,4 @@ namespace Rlx
         public ResultTask<TValue, Unit> ConsumeError(Func<TError, Task> fn) =>
             MapError(error => fn(error).Select(() => Unit.Value));
     }
-
-    public static class ResultExtensions
-    {
-        public static T UnwrapEither<T>(this Result<T, T> result) =>
-            result.UnwrapOrElse(error => error);
-
-        public static Task<T> UnwrapEitherAsync<T>(this ResultTask<T, T> result) =>
-            result.UnwrapOrElseAsync(error => error);
-    }
 }
---
+++ src/Rlx/ResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Rlx
+{
+    public static class ResultExtensions
+    {
+        public static T UnwrapEither<T>(this Result<T, T> result) =>
+            result.UnwrapOrElse(error => error);
+
+        public static Task<T> UnwrapEitherAsync<T>(this ResultTask<T, T> result) =>
+            result.UnwrapOrElseAsync(error => error);
+    }
+}
