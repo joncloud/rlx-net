1323438bfc571c9c23840f215972172dc3d68855
joncloud <jdberube@gmail.com>
2018-03-25 20:43:20 -0700

    Slight modification

--- pack.sh
+++ pack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-VERSION_SUFFIX=$1
+VALUE=$1
 
 if [ -z $VERSION_SUFFIX ]; then
   VERSION=$(grep -Po ./src/Rlx/Rlx.csproj -e '(?<=<VersionPrefix>)[^<]+')
   echo "Using Version $VERSION"
   dotnet pack --configuration=Release /property:Version=$VERSION
 else
-  echo "Using Version Suffix $VERSION_SUFFIX"
-  dotnet pack --configuration=Release --version-suffix="$VERSION_SUFFIX"
+  echo "Using Version Suffix $VALUE"
+  dotnet pack --configuration=Release --version-suffix="$VALUE"
 fi
